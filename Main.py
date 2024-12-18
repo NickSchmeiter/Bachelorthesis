3c8ab14df5219b106bd4bddda2a6fbdc29df0b4b
nickschmeiter <nickschmeiter98@gmail.com>
2024-12-18 14:26:39 +0100

    tweets and likes more or less

--- Main.py
+++ Main.py
@@ -2,12 +2,58 @@ from mycustomLLM import LLM
 from langchain_ollama import OllamaLLM
 from customagents import agents
 
-model = OllamaLLM(model="llama3.2")
-agent = LLM(model=model)
+# Fragen an Hochstein: 
+
+# Wie und welche Datenbank am besten einbinden? 
+
+# Llama lokal dumm, gut oder soll ich selber weiter recherschieren ? 
+
+# Scope des Projekts (wie viele user simulieren ?)
+                    
+# Welche funktionen (tweet, like, comment) genug ? 
+
+tulfirstuser={}
+tulseconduser={}
+
+
+def showtweettoagent(tweet:str,agent:agents,tweetandlikes:dict):
+    if agent.evaluatetweet(tweet)=="like":
+        tweetandlikes[tweet]+=1
+
 firstuser=agents(age=26,gender='male',nationality='Germany',job='computer science student',
                  interest='Skateboarding',location='Berlin',politicalcompass='leftliberal')
 firstuser.setbackground()
-test=firstuser.tweet()
-print(test)
-test=firstuser.tweet()
-print(test)
+
+seconduser=agents(age=27,gender='female',nationality='half italian, quarter japanese, quarter peruvian',
+                  job='marketing at edgless systems(Berliner startup for cybersecurity)',
+                 interest='books, food, photography, drugs',location='Berlin',politicalcompass='leftliberal')
+seconduser.setbackground()
+
+
+
+
+# tweet1=firstuser.tweet()
+# tulfirstuser[tweet1]=0
+# print(tweet1)
+# tweet2=firstuser.tweet()
+# tulfirstuser[tweet2]=0
+# print(tweet2)
+
+tweet1=seconduser.tweet()
+tulseconduser[tweet1]=0
+print(tweet1)
+tweet2=seconduser.tweet()
+tulseconduser[tweet2]=0
+print(tweet2)
+
+# for key in tulfirstuser.keys():
+#     showtweettoagent(key,seconduser,tulfirstuser)
+#     print(tulfirstuser[key])
+
+#show all tweets to first user and get likes or dislikes
+for key in tulseconduser.keys():
+    showtweettoagent(key,firstuser,tulseconduser)
+    print(tulseconduser[key])
+
+
+    
--- customagents.py
+++ customagents.py
@@ -11,15 +11,27 @@ class agents(object):
         self.politicalcompass = politicalcompass    
         self.memory=[]
         self.LLM=LLM(model=OllamaLLM(model="llama3.2"))
-    def tweet(self)->str:
-        standardtweet="""This is your prompt: Please make a twitter tweet according to your background and memory.
-                      The tweet should be in english. The tweet should make sense according to your memory.
-                      Tweet like you are a heavy twitter user so dont focus too much on your background in each single tweet.
 
-                      Remember tweets have a maximum of 280 characters
-                      and usually they use hashtags # and mentions @. 
-                      Just post the tweet and nothing else. Nothing preeceding or following the tweet."""
-        return self.prompt(standardtweet)
+
+    #sets background for the agent so agent get context for what he can tweet about 
+    # and adds it to memory of the agent
+    def setbackground(self):
+        backgroundtext="This is your background. You are a " +str(self.age)+ " years old, your gender is "+self.gender+" youre nationality is "+self.nationality+" and your job is "+self.job+" your interest is "+self.interest
+        backgroundtext=backgroundtext+" You are currently located in "+self.location+" and your political compass is "+self.politicalcompass
+        self.add_memory(backgroundtext)
+
+
+    #adds string to memorylist so agent can recall it later
+    def add_memory(self, text:str):
+        self.memory.append(text)
+
+
+    #returns memorylist
+    def get_memory(self)->list:
+        return self.memory
+    
+
+    #recalls memory and then adds the text and prompts this to the LLM and returns an answer
     def prompt(self, text)->str:
 
         total=""
@@ -34,12 +46,24 @@ class agents(object):
         self.add_memory(result) #add output to memory
         return result
     
-    def setbackground(self):
-        backgroundtext="This is your background. You are a " +str(self.age)+ " years old, your gender is "+self.gender+" youre nationality is "+self.nationality+" and your job is "+self.job+" your interest is "+self.interest
-        backgroundtext=backgroundtext+" You are currently located in "+self.location+" and your political compass is "+self.politicalcompass
-        self.add_memory(backgroundtext)
+    #prompts a generic tweet
+    def tweet(self)->str:
+        standardtweet="""This is your prompt: Please make a twitter tweet according to your background and memory.
+                      Your tweet should be as realistic as possible so dont overestimate your background.  
+                      The tweet should be in english. The tweet should make sense according to your memory.
+                      Tweet like you are a heavy twitter user so dont focus too much on your background in each single tweet.
 
-    def add_memory(self, text:str):
-        self.memory.append(text)
-    def get_memory(self)->list:
-        return self.memory
+                      Remember tweets have a maximum of 280 characters
+                      and usually they use hashtags # and mentions @. 
+                      Just post the tweet and nothing else. Nothing preeceding or following the tweet."""
+        return self.prompt(standardtweet)
+    
+    def evaluatetweet(self,tweet)->str:
+        prompt="""This is your prompt: Please evaluate the tweet which will follow after this prompt.
+                  You should evaluate if you would give the tweet a like from a twitter user perspective.
+                  remember your background and memory and evaluate the tweet according to that.
+                  If you would like the tweet, just return 'like' if you would not like the tweet return 'dislike'
+                  Dont return anything else your answer should really just be 'like' or 'dislike'.
+                  HERE COMES THE TWEET: \n"""
+        prompt=prompt+tweet
+        return self.prompt(prompt)
