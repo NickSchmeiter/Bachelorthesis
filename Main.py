ee2dc95951ea831e36330779fb5760479af9baea
nickschmeiter <nickschmeiter98@gmail.com>
2024-12-23 15:12:53 +0100

    database tests

--- Main.py
+++ Main.py
@@ -1,17 +1,47 @@
 from mycustomLLM import LLM
 from langchain_ollama import OllamaLLM
 from customagents import agents
+import sqlite3
+
+
 
 # Fragen an Hochstein: 
 
-# Wie und welche Datenbank am besten einbinden? 
+# Wie und welche Datenbank am besten einbinden?
+# SQlite oder postgres  
 
-# Llama lokal dumm, gut oder soll ich selber weiter recherschieren ? 
+# Llama lokal dumm, gut oder soll ich selber weiter recherschieren ?
+# model ist plug and play und das is gut. in der arbeit llama 3.2 begründen. 
 
 # Scope des Projekts (wie viele user simulieren ?)
+# erstmal implementieren und dann gucken wie realistisch das wird 
                     
-# Welche funktionen (tweet, like, comment) genug ? 
+# Welche funktionen (tweet, like, comment) genug ?
+# erstmal implementieren und dann eventuell funktionen nachbauen wenn es unrealistisch klingt  
 
+#memory function selber implementieren oder nicht ?
+#eventuell mit relevanz lösen oder mit vektordatenbank mit embeddings von tweets dann nimmt man nur tweets die vom vektor nah drin sind. chroma db in langchain drinnen
+# 
+# prompt engineering ?
+# sowohl mehr research als auch frei schnautze probieren. 
+#  
+
+#database creation test
+
+conn = sqlite3.connect('twitter.db')
+
+c = conn.cursor()
+
+"""c.execute(""CREATE TABLE tweets (
+            tweet_id integer primary key,
+            user_id integer,
+            tweet text,
+            likes integer
+            )"")"""
+
+c.execute("""INSERT INTO tweets VALUES ('1','2','This is a test tweet', '0')""")
+conn.commit()
+conn.close()
 
 
 tulfirstuser={}
